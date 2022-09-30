59f7689f005bf98c28ffe7c5f9ddb52e325bf9d6
Bojan <veljanoski_b@yahoo.com>
2022-09-30 14:20:44 +0200

    Refactor

--- cypress/integration/textComponent.feature
+++
@@ -1,7 +0,0 @@
-Feature: Text Component
-
-  Scenario: Text Component Tests
-
-    Given User opens the Text Component Page
-
-    And Checks the Right Alignment
--- cypress/integration/titleComponent/titleComponentSteps.js
+++ cypress/integration/titleComponent/titleComponentSteps.js
@@ -2,7 +2,7 @@ import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps';
 import { getBaseUrl, getEnv } from "../../config/config";
 import { isSbreakPoint, isMbreakPoint, isLbreakPoint, isXLbreakPoint } from "../../support/utils"
 import describeBuilder from '../../support/describeBuilder'
-import { titleComponentSelectors } from './titleComponentSelectors.js';
+import { ALIGNMENTS, title } from './titleComponentSelectors.js';
 
 
 const describeConstructor = new describeBuilder()
@@ -11,112 +11,109 @@ describe(describeConstructor.size(Object.values) + " " + 'Title Component', func
 
   // Checking of the Alignment of the Title Component
 
-  Given('User opens the Title Component Page', () => {
+  Given('I open the Title Component Page', () => {
 
     cy.visit(getBaseUrl().concat('/en-aa-sample/editorial/general-components/title-component'));
 
   });
-  // checks the Left Alignment
+
   And('I check the Left Alignment', () => {
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.leftAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.LEFT)).should('be.visible')
     }
 
     if (isMbreakPoint() || isSbreakPoint()) {
-      cy.get(titleComponentSelectors.title.leftAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.LEFT)).should('be.visible')
     }
   })
 
   And('I check the Right Alignment', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.rightAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.RIGHT)).should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.rightAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.RIGHT)).should('be.visible')
     }
   })
 
   Then('I check the Center Alignment', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.centerAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.CENTER)).should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.centerAlignment).should('be.visible')
+      cy.get(title.alignment(ALIGNMENTS.CENTER)).should('be.visible')
     }
   })
 
-
-
-
-  // Checking of the Font Size of the Title Component-Heading
+  // Checking the Heading of the Title Component-Heading
 
   And('I check the Heading 1', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading1).contains('Title Heading H1').should('be.visible')
+      cy.get(title.heading(1)).contains('Title Heading H1').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading1).contains('Title Heading H1').should('be.visible')
+      cy.get(title.heading(1)).contains('Title Heading H1').should('be.visible')
     }
   })
 
   And('I check the Heading 2', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading2).contains('Title Heading H2').should('be.visible')
+      cy.get(title.heading(2)).contains('Title Heading H2').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading2).contains('Title Heading H2').should('be.visible')
+      cy.get(title.heading(2)).contains('Title Heading H2').should('be.visible')
     }
   })
 
   And('I check the Heading 3', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading3).contains('Title Heading H3').should('be.visible')
+      cy.get(title.heading(3)).contains('Title Heading H3').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading3).contains('Title Heading H3').should('be.visible')
+      cy.get(title.heading(3)).contains('Title Heading H3').should('be.visible')
     }
   })
 
   And('I check the Heading 4', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading4).contains('Title Heading H4').should('be.visible')
+      cy.get(title.heading(4)).contains('Title Heading H4').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading4).contains('Title Heading H4').should('be.visible')
+      cy.get(title.heading(4)).contains('Title Heading H4').should('be.visible')
     }
   })
 
   And('I check the Heading 5', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading5).contains('Title Heading H5').should('be.visible')
+      cy.get(title.heading(5)).contains('Title Heading H5').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading5).contains('Title Heading H5').should('be.visible')
+      cy.get(title.heading(5)).contains('Title Heading H5').should('be.visible')
     }
   })
 
   Then('I check the Heading 6', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading6).contains('Title Heading H6').should('be.visible')
+      cy.get(title.heading(6)).contains('Title Heading H6').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading6).contains('Title Heading H6').should('be.visible')
+      cy.get(title.heading(6)).contains('Title Heading H6').should('be.visible')
     }
   })
 
@@ -125,11 +122,11 @@ describe(describeConstructor.size(Object.values) + " " + 'Title Component', func
   Then('I check the Heading Negative', () => {
 
     if (isXLbreakPoint() || isLbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading1).contains('Title Heading H2').should('be.visible')
+      cy.get(title.heading(1)).contains('Title Heading H2').should('be.visible')
     }
 
     if (isSbreakPoint() || isMbreakPoint()) {
-      cy.get(titleComponentSelectors.title.heading1).contains('Title Heading H2').should('be.visible')
+      cy.get(title.heading(1)).contains('Title Heading H2').should('be.visible')
     }
   })
 })
